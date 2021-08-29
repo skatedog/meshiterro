69f512af209c80c36974d65568ea3933f6311744
skatedog <skatedog0001@gmail.com>
2021-08-29 03:27:16 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '4b5151037593fda2b609657b04d5ebf83b22cfd858ea7c8a553aabb2d219781afd6fbf06031eee6808fb437ace70eaa4f6a223d3e190eb6422a5361db6a40176'
